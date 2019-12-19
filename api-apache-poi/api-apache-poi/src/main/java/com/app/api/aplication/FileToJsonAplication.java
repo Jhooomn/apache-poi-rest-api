48c636c8d126eebd389870b9ffb31e04e6feddfb
Jhon Baron <jhooomn@gmail.com>
2019-12-19 16:51:43 -0500

    stable version

--- api-apache-poi/api-apache-poi/src/main/java/com/app/api/aplication/FileToJsonAplication.java
+++ api-apache-poi/api-apache-poi/src/main/java/com/app/api/aplication/FileToJsonAplication.java
@@ -37,16 +37,23 @@ public class FileToJsonAplication {
 		File tempFile = tempDir.resolve(file.getOriginalFilename()).toFile();
 		file.transferTo(tempFile);
 		Workbook workbook = WorkbookFactory.create(tempFile);
+		System.out.println("HERE: " + workbook.getNumberOfSheets());
 
-		return this.workBookToJson(workbook);
+		if (workbook.getNumberOfSheets() > 0) {
+			return this.nSheetsWorkBookToJson(workbook);
+		} else {
+			return this.oneSheetWorkBookToJson(workbook);
+		}
 
 	}
 
-	public List<Object> workBookToJson(Workbook workbook) {
+	public List<Object> nSheetsWorkBookToJson(Workbook workbook) {
 		List<Object> list = new ArrayList<>();
 
-		for (int i = 0; i < workbook.getNumberOfSheets(); i++) {
+		int i = 0;
+		while (i < workbook.getNumberOfSheets()) {
 			Sheet sheet = workbook.getSheetAt(i);
+
 			Supplier<Stream<Row>> rowStreamSupplier = fileToJsonMapper.getRowStreamSupplier(sheet);
 			Row headerRow = rowStreamSupplier.get().findFirst().get();
 
@@ -71,7 +78,39 @@ public class FileToJsonAplication {
 				return fileToJsonMapper.cellIteratorSupplier(colCount).get().collect(Collectors
 						.toMap(x -> headerCells.get(x), y -> cellList.get(y), (oldValue, newValue) -> newValue));
 			}).collect(Collectors.toList()));
+			i++;
 		}
+		return list;
+	}
+
+	public List<Object> oneSheetWorkBookToJson(Workbook workbook) {
+		List<Object> list = new ArrayList<>();
+		Sheet sheet = workbook.getSheetAt(0);
+		Supplier<Stream<Row>> rowStreamSupplier = fileToJsonMapper.getRowStreamSupplier(sheet);
+		Row headerRow = rowStreamSupplier.get().findFirst().get();
+
+		Iterator it = sheet.iterator();
+		while (it.hasNext()) {
+			Row row = (Row) it.next();
+			for (int cn = 0; cn < row.getLastCellNum(); cn++) {
+				Cell cell = row.getCell(cn, Row.MissingCellPolicy.CREATE_NULL_AS_BLANK);
+			}
+		}
+
+		List<String> headerCells = StreamSupport.stream(headerRow.spliterator(), false).map(c -> c.toString())
+				.collect(Collectors.toList());
+
+		int colCount = headerCells.size();
+
+		list.add(rowStreamSupplier.get().skip(1).map(row -> {
+
+			List<String> cellList = StreamSupport.stream(row.spliterator(), false).map(c -> c.toString())
+					.collect(Collectors.toList());
+
+			return fileToJsonMapper.cellIteratorSupplier(colCount).get().collect(
+					Collectors.toMap(x -> headerCells.get(x), y -> cellList.get(y), (oldValue, newValue) -> newValue));
+		}).collect(Collectors.toList()));
+
 		return list;
 	}
 
